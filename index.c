69860093416b232f0222c41acd830e43268205a5
AlanLucasSC <AlanLucasSC2011@gmail.com>
2019-07-19 18:37:56 +0000

    Criando e Load de arquivos

--- index.c
+++ index.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <windows.h>
+#include <stdlib.h>
 
 int main(){
 	CreateDirectory("C:/Users/alcastro/Documents/ProcessTask/c-files/batata", NULL);
+	system("cd C:/Users/alcastro/Documents/ProcessTask/c-files/batata");
+	system("git clone https://github.com/AlanLucasSC/csv.git");
 	printf("Batata");
 	return 0;
 }

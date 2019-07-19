16a6b1305dc8205e3e5ba80f953ad33810bc16c7
AlanLucasSC <AlanLucasSC2011@gmail.com>
2019-07-19 17:57:47 +0000

    Criando uma pasta

--- index.c
+++ index.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+struct stat st = {0};
 
 int main(){
-	printf("Batata\n");
+	if(stat("./batata", &st) == -1){
+		mkdir("./batata", 0700);
+	}
+	printf("Batata");
 	return 0;
 }
